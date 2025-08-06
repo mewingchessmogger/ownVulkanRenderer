321bc8ede73bd6bb0861cc6cff90ae661de776e4
Gustav Andersson <159704765+mewingchessmogger@users.noreply.github.com>
2025-08-06 18:50:59 +0200

    triangle with blue shifting background

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <vulkan/vulkan.hpp>
 #include <VkBootstrap.h>
 #include <GLFW/glfw3.h>
@@ -170,6 +171,33 @@ void initCommandBuffers() {
 
 }
 
+std::vector<char> readFile(const std::string& fileName) {
+	std::ifstream file(fileName, std::ios::ate | std::ios::binary);//set pointer at end of file
+	if (!file.is_open()) {
+		throw std::runtime_error("failed to open file!");
+	}
+	size_t fileSize = (size_t)file.tellg();//gets length of file cuz its telling where the pointer is
+	std::vector<char> buffer(fileSize);//allocate exact length
+	file.seekg(0);//resets pointer to start of file
+	file.read(buffer.data(), fileSize);//fills up vector
+	file.close();
+	std::cout << fileSize << "!!!!!!!!\n";
+	return buffer;
+}
+
+
+
+vk::ShaderModule createShaderModule(const std::vector<char>& code) {
+	vk::ShaderModuleCreateInfo createInfo{};
+	createInfo.codeSize = code.size();
+	createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+	vk::ShaderModule shaderModule;
+	shaderModule = _device.createShaderModule(createInfo);
+	
+	return shaderModule;
+}
+
+
 void initSyncs(){
 	
 	vk::FenceCreateInfo createInfo{};
@@ -188,40 +216,137 @@ void initSyncs(){
 }
 
 
-void initPipeline() {
+
+void initGraphicsPipeline() {
+
+	
 	vk::PipelineRenderingCreateInfo dynRenderInfo{};
-	dynRenderInfo.setColorAttachmentCount(1)
-				 .setPColorAttachmentFormats(&_swapchainFormat);
+	dynRenderInfo
+		.setColorAttachmentCount(1)
+		.setPColorAttachmentFormats(&_swapchainFormat);
+			     
+
+	auto vertexCode = readFile("shaders/vertex/firstVertex.spv");
+	auto fragCode = readFile("shaders/frag/firstFrag.spv");
+	_vertexShader = createShaderModule(vertexCode);
+	_fragShader = createShaderModule(fragCode);
+	vk::PipelineShaderStageCreateInfo vertexInfo{};
+	vertexInfo
+		.setStage(vk::ShaderStageFlagBits::eVertex)
+		.setModule(_vertexShader)
+		.setPName("main");
+	vk::PipelineShaderStageCreateInfo fragInfo{};
+	fragInfo
+		.setStage(vk::ShaderStageFlagBits::eFragment)
+		.setModule(_fragShader)
+		.setPName("main");
+	vk::PipelineShaderStageCreateInfo shaderStages[2] = { vertexInfo,fragInfo };
+
+	vk::PipelineVertexInputStateCreateInfo vertexInputInfo{};
 
 
 
-	vk::GraphicsPipelineCreateInfo createInfo{};
-	createInfo.setPNext(dynRenderInfo)			// setting or dynrendering
-			  .setFlags(vk::PipelineCreateFlags{})// tweakiung pipelines set to nothing
-			  .setStageCount (1)			// tells vulkan how many shader stages hooked up
-			  .setPStages(nullptr)
-			  
-			  .setPVertexInputState(nullptr)
-			  .setPInputAssemblyState(nullptr)
-			  .setPTessellationState(nullptr)
-			  .setPViewportState(nullptr)
-			  .setPRasterizationState(nullptr)
-			  .setPMultisampleState(nullptr)
-			  .setPDepthStencilState(nullptr)
-			  .setPColorBlendState(nullptr)
-			  .setPDynamicState(nullptr)
-			  .setLayout(vk::PipelineLayout{})
-			  .setRenderPass(nullptr)
-			  .setSubpass(0)
-			  .setBasePipelineHandle(vk::Pipeline{})
-			  .setBasePipelineIndex(-1);
-		
-	_graphicsPipeline = _device.createGraphicsPipeline(VK_NULL_HANDLE, createInfo).value;
+	vk::PipelineInputAssemblyStateCreateInfo assInfo{};
+	assInfo.setTopology(vk::PrimitiveTopology::eTriangleList);
+
+
+	vk::PipelineViewportStateCreateInfo viewportInfo{};
+	vk::Rect2D scissor{};
+	scissor
+		.setOffset(vk::Offset2D{ 0,0 })
+		.setExtent(_swapchainExtent);
+	vk::Viewport viewport{};
+	viewport
+		.setX(0)
+		.setY(0)
+		.setWidth((float)_swapchainExtent.width)
+		.setHeight((float)_swapchainExtent.height)
+		.setMinDepth(0.0)
+		.setMaxDepth(1.0);
+	viewportInfo
+		.setScissorCount(1)
+		.setViewportCount(1);
+	
+
+	vk::PipelineRasterizationStateCreateInfo razInfo{};
+	razInfo
+		.setDepthClampEnable(vk::False)
+		.setRasterizerDiscardEnable(vk::False)
+		.setPolygonMode(vk::PolygonMode::eFill)
+		.setCullMode(vk::CullModeFlagBits::eBack)
+		.setFrontFace(vk::FrontFace::eClockwise)
+		.setDepthBiasEnable(vk::False)
+		.setDepthBiasSlopeFactor(1.0)
+		.setLineWidth(1.0);
+
+
+	vk::PipelineMultisampleStateCreateInfo multisampleInfo{};
+	multisampleInfo
+		.setRasterizationSamples(vk::SampleCountFlagBits::e1)
+		.setSampleShadingEnable(vk::False);
+
+
+	vk::PipelineDepthStencilStateCreateInfo stencilInfo{};//nullptr
+	
+
+	vk::PipelineColorBlendStateCreateInfo blendInfo{};
+	vk::PipelineColorBlendAttachmentState attachInfo{};
+	attachInfo.colorWriteMask =
+		vk::ColorComponentFlagBits::eR |
+		vk::ColorComponentFlagBits::eG |
+		vk::ColorComponentFlagBits::eB |
+		vk::ColorComponentFlagBits::eA;
+	attachInfo.blendEnable = vk::True;
+	attachInfo.srcColorBlendFactor = vk::BlendFactor::eSrcAlpha;
+	attachInfo.dstColorBlendFactor = vk::BlendFactor::eOneMinusSrcAlpha;
+	attachInfo.colorBlendOp = vk::BlendOp::eAdd;
+	attachInfo.srcAlphaBlendFactor = vk::BlendFactor::eOne;
+	attachInfo.dstAlphaBlendFactor = vk::BlendFactor::eZero;
+	attachInfo.alphaBlendOp = vk::BlendOp::eAdd;
+	blendInfo.setAttachments(attachInfo);
+
+	
+	std::vector dynamicStates = {
+	vk::DynamicState::eViewport,
+	vk::DynamicState::eScissor
+	};
+	vk::PipelineDynamicStateCreateInfo dynamicState{};
+	dynamicState.setDynamicStates(dynamicStates);
+
+
+	vk::PipelineLayoutCreateInfo layoutInfo{};
+	vk::PipelineLayout layout = _device.createPipelineLayout(layoutInfo);
+	
+
+
+
+	vk::GraphicsPipelineCreateInfo createPipelineInfo{};
+	createPipelineInfo
+		.setPNext(&dynRenderInfo)			// setting or dynrendering
+		.setFlags(vk::PipelineCreateFlags{})// tweakiung pipelines set to nothing
+		.setStages(shaderStages)// c arr of shaderstageInfos
+		.setPVertexInputState(&vertexInputInfo)// setting up buffers future
+		.setPInputAssemblyState(&assInfo)// setting up that interpereting every three oints as a triangle here
+		.setPViewportState(&viewportInfo)
+		.setPRasterizationState(&razInfo)
+		.setPMultisampleState(&multisampleInfo)
+		.setPDepthStencilState(nullptr)
+		.setPColorBlendState(&blendInfo)
+		.setPDynamicState(&dynamicState)
+		.setLayout(layout)
+		.setBasePipelineHandle(vk::Pipeline{})
+		.setBasePipelineIndex(-1);
+			
+	_graphicsPipeline = _device.createGraphicsPipeline({}, createPipelineInfo).value;
+
+
 
 
 }
 	
+void initComputePipeline() {
 
+}
 	
 
 
@@ -294,26 +419,35 @@ void transitionImage(vk::Image image,vk::CommandBuffer cmdBuffer, vk::ImageLayou
 
 void draw() {
 	static float counter{};
-	counter += 1/120.0f;
+	counter += 0.01;
 	
+
+	//SYNC!!!!!!!!{
 	currentFrame= (currentFrame + 1) % NUM_OF_IMAGES;
 	vk::Fence curFence[] = { _fences[currentFrame] };
 
 	_device.waitForFences(1,curFence, vk::True, 1000000000);
 	_device.resetFences(curFence);
 
-
+	
 	
 
-	vk::Semaphore imageReadySemaph = _imageReadySemaphores[currentFrame];
-	
+	vk::Semaphore imageReadySemaph = _imageReadySemaphores[currentFrame];//read below same shtick
+	vk::CommandBuffer cmdBuffer = _cmdBuffers[currentFrame];//this is indx currentFrame cuz the fence above 
+															//guarantees buffer is done submitting, therefore choose buffer based on fences index"currentFrame"
+
 
 	imageIndex = _device.acquireNextImageKHR(_swapchain, 1000000000, imageReadySemaph).value;
 
-	vk::CommandBuffer cmdBuffer = _cmdBuffers[currentFrame];
+
+
+	//now were using imageindex as index, swapchainimages using it is obvious as that is the currently free from rendering
+	//we use renderfinished semaphore to signal finished rendering the image, therefore it should be tighlty coupled to same index as swapchainimages
+	// "use currently free imagiendex to the image and its semaphore, otherwise the problem might be you reuse semaphore that is already being used in waitsemaphore field in presentsrckhr eg race condition
 	vk::Semaphore renderFinishedSemaph = _renderFinishedSemaphores[imageIndex];
 	vk::Image curImage = _swapchainImages[imageIndex];
 	
+	//SYNC!!!!!!!!}
 
 
 	cmdBuffer.reset();
@@ -326,24 +460,55 @@ void draw() {
 	
 	transitionImage(curImage,cmdBuffer, vk::ImageLayout::eUndefined, vk::ImageLayout::eGeneral);
 	
-	//begin dynrendering
-
+	
 	vk::ClearColorValue clr{};
 	
 	clr.setFloat32({ 0.0f, 0.0f, sinf(counter), 1.0f});
 
 	vk::ImageSubresourceRange subRange{};
 	subRange.setAspectMask(vk::ImageAspectFlagBits::eColor)
-		.setLayerCount(vk::RemainingArrayLayers)
-		.setBaseMipLevel(0)
-		.setLevelCount(vk::RemainingMipLevels)
-		.setBaseArrayLayer(0);
+			.setLayerCount(vk::RemainingArrayLayers)
+			.setBaseMipLevel(0)
+			.setLevelCount(vk::RemainingMipLevels)
+			.setBaseArrayLayer(0);
 
 
 
-	cmdBuffer.clearColorImage(curImage, vk::ImageLayout::eGeneral, clr,subRange);
+	//cmdBuffer.clearColorImage(curImage, vk::ImageLayout::eGeneral, clr,subRange);
 
-	transitionImage(curImage, cmdBuffer, vk::ImageLayout::eGeneral, vk::ImageLayout::ePresentSrcKHR);
+	transitionImage(curImage, cmdBuffer, vk::ImageLayout::eGeneral, vk::ImageLayout::eColorAttachmentOptimal);
+	//begin dynrendering
+	vk::RenderingAttachmentInfo attachInfo{};
+	attachInfo
+		.setImageView(_swapchainImageViews[imageIndex])
+		.setImageLayout(vk::ImageLayout::eColorAttachmentOptimal)
+		.setClearValue(clr)
+		.setLoadOp(vk::AttachmentLoadOp::eClear)
+		.setStoreOp(vk::AttachmentStoreOp::eStore);
+		
+
+
+	//attachInfo.set
+	vk::RenderingInfoKHR renderInfo{};
+	vk::Rect2D area;
+	area.setOffset(vk::Offset2D(0,0)).setExtent(_swapchainExtent);
+	renderInfo
+		.setRenderArea(area)
+		.setLayerCount(1)
+		.setColorAttachments(attachInfo);
+
+	cmdBuffer.beginRendering(renderInfo);
+	
+
+	cmdBuffer.bindPipeline(vk::PipelineBindPoint::eGraphics, _graphicsPipeline);
+	//setViewport(0, vk::Viewport(0.0f, 0.0f, static_cast<float>(swapChainExtent.width), static_cast<float>(swapChainExtent.height), 0.0f, 1.0f));
+	//setScissor(0, vk::Rect2D(vk::Offset2D(0, 0), swapChainExtent));
+	cmdBuffer.setViewport(0,vk::Viewport(0.0f,0.0f,float(_swapchainExtent.width),float(_swapchainExtent.height),0.0f,1.0f));
+	cmdBuffer.setScissor(0,vk::Rect2D(vk::Offset2D(0,0),_swapchainExtent));
+	cmdBuffer.draw(3, 1, 0, 0);
+	cmdBuffer.endRendering();
+
+	transitionImage(curImage, cmdBuffer, vk::ImageLayout::eColorAttachmentOptimal, vk::ImageLayout::ePresentSrcKHR);
 
 
 	cmdBuffer.end();
@@ -367,14 +532,18 @@ void draw() {
 
 	
 	
-	subInfo.setWaitSemaphoreInfos(waitInfo).setSignalSemaphoreInfos(signalInfo).setCommandBufferInfos(cmdInfo);
+	subInfo.setWaitSemaphoreInfos(waitInfo)
+		   .setSignalSemaphoreInfos(signalInfo)
+		   .setCommandBufferInfos(cmdInfo);
 	
 	
 	
 	_graphicsQueue.handle.submit2(subInfo,_fences[currentFrame]);
 
 	vk::PresentInfoKHR presInfo{};
-	presInfo.setWaitSemaphores({ renderFinishedSemaph}).setSwapchains(_swapchain).setImageIndices({ imageIndex })	;
+	presInfo.setWaitSemaphores({ renderFinishedSemaph})
+			.setSwapchains(_swapchain)
+			.setImageIndices({ imageIndex });
 	
 	_graphicsQueue.handle.presentKHR(presInfo);
 	
@@ -389,9 +558,15 @@ int main() {
 	initSwapchain();
 	initCommandBuffers();
 	initSyncs();
-	//initPipeline();
+	initGraphicsPipeline();
+	//initComputePipeline
+
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+			glfwSetWindowShouldClose(window, GLFW_TRUE);
+		}
+
 		draw();
 	}
 	cleanup();
